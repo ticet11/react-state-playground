7f380a9eb8ab1f3a3abd02ce20f238becbb47fa3
Brian Kozub <ticet11@gmail.com>
2020-05-27 12:03:42 -0600

    Font Sizer and Toggle pages functioning.

--- src/pages/FontSizer.js
+++ src/pages/FontSizer.js
@@ -1,13 +1,40 @@
 //TODO: Increase/decrease font size
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class FontSizer extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            fontSize: 18,
+        };
+    }
+
+    handleUpClick = () => {
+        this.setState({
+            fontSize: this.state.fontSize + 2
+        })
+    }
+
+    handleDownClick = () => {
+        this.setState({
+            fontSize: this.state.fontSize - 2
+        })
+    }
+    
+
     render() {
         return (
             <div>
                 <h1>Font Sizer</h1>
+                <button onClick={() => this.handleDownClick()}>-</button>
+                <button onClick={() => this.handleUpClick()}>+</button>
+                <h2 style={{ fontSize: this.state.fontSize }}>
+                    Font Size is {this.state.fontSize}px.
+                </h2>
             </div>
-        )
+        );
     }
 }
+
--- src/pages/Toggle.js
+++ src/pages/Toggle.js
@@ -1,13 +1,29 @@
 // TODO: Toggle between hello and goodbye
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Toggle extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            toggleOn: true,
+        };
+    }
+
+    handleClick = () => {
+        this.setState({
+            toggleOn: !this.state.toggleOn,
+        });
+    };
+
     render() {
         return (
             <div>
                 <h1>Toggle</h1>
+                <button onClick={() => this.handleClick()} >Click to Toggle</button>
+                <h2>{this.state.toggleOn ? "Hello" : "Goodbye"}</h2>
             </div>
-        )
+        );
     }
 }

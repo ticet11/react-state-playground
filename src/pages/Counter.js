6c0d2b1a824245dd42a7dfd944a2b39f09a95377
Brian Kozub <ticet11@gmail.com>
2020-05-28 10:02:28 -0600

    Refactored counter page to use one handleClick function and added basic styles.

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -1,36 +1,42 @@
 //TODO: State value -> increase and decrease state value
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Counter extends Component {
     constructor() {
         super();
 
         this.state = {
-            stateNum: 0
-        }
+            stateNum: 0,
+        };
     }
 
-    handleUpClick = () => {
+    handleButtonClick = (number) => {
         this.setState({
-            stateNum: this.state.stateNum + 1
-        })
-    }
-
-    handleDownClick = () => {
-        this.setState({
-            stateNum: this.state.stateNum - 1
-        })
-    }
+            stateNum: this.state.stateNum + number,
+        });
+    };
 
     render() {
         return (
-            <div>
+            <div style={{ padding: '10px' }}>
                 <h1>Counter</h1>
-                <h2>{this.state.stateNum}</h2>
-                <button onClick={() => this.handleUpClick()} >Up</button>
-                <button onClick={() => this.handleDownClick()} >Down</button>
+                <div className="counter-container" style={{ width: '100px', padding: '5px', display: 'grid', gridTemplateColumns: '1fr 1fr', alignItems: 'center' }}>
+                    <h2>{this.state.stateNum}</h2>
+                    <div className="button-container">
+                        <button
+                            onClick={() => this.handleButtonClick(1)}
+                        >
+                            Up
+                        </button>
+                        <button
+                            onClick={() => this.handleButtonClick(-1)}
+                        >
+                            Down
+                        </button>
+                    </div>
+                </div>
             </div>
-        )
+        );
     }
 }

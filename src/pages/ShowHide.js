44eb6fe46563641c7ca7be7f31c6d68ff6b9b50b
Brian Kozub <ticet11@gmail.com>
2020-05-29 10:53:24 -0600

    Small style fix in showHide.

--- src/pages/ShowHide.js
+++ src/pages/ShowHide.js
@@ -22,28 +22,42 @@ export default class ShowHide extends Component {
             <div>
                 <h1>Show Hide</h1>
                 {this.state.hidden ? (
-                        <div>
-                            <h2>What have you done?!</h2>
-                            <button onClick={() => this.handleClick()}>
-                                Secret Feature
-                            </button>
-                        </div>
-                    ) : (
-                        <div style={{display: 'flex', flexDirection: 'column'}}>
-                            <h2>Don't click that button!</h2>
-                            <img
-                                style={{ width: "100px" }}
-                                src="https://t4.rbxcdn.com/d62e9c8c8495d3e3a6213a0788c4ae40"
-                                alt="Guy Fieri welcoming you to flavor town"
-                            />
-                            <button style={{width: '100px', margin: '5px'}} onClick={() => this.handleClick()}>
-                                Secret Feature
-                            </button>
-                        </div>
-                    )}
-                </div>
-                
-            
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                        }}
+                    >
+                        <h2>What have you done?!</h2>
+                        <button
+                            style={{ width: "100px", margin: "5px" }}
+                            onClick={() => this.handleClick()}
+                        >
+                            Secret Feature
+                        </button>
+                    </div>
+                ) : (
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                        }}
+                    >
+                        <h2>Don't click that button!</h2>
+                        <img
+                            style={{ width: "100px" }}
+                            src="https://t4.rbxcdn.com/d62e9c8c8495d3e3a6213a0788c4ae40"
+                            alt="Guy Fieri welcoming you to flavor town"
+                        />
+                        <button
+                            style={{ width: "100px", margin: "5px" }}
+                            onClick={() => this.handleClick()}
+                        >
+                            Secret Feature
+                        </button>
+                    </div>
+                )}
+            </div>
         );
     }
 }

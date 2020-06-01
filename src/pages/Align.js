7435a99ccbf307906a3247d3625c5bce19566d5b
Brian Kozub <ticet11@gmail.com>
2020-06-01 13:35:58 -0600

    Align refactored to align image instead of text.

--- src/pages/Align.js
+++ src/pages/Align.js
@@ -2,6 +2,8 @@
 
 import React, { Component } from "react";
 
+import guyAlign from "../images/guyAlign.jpg";
+
 export default class Align extends Component {
     constructor() {
         super();
@@ -10,45 +12,55 @@ export default class Align extends Component {
             style: {
                 width: "80vw",
                 border: "5px solid green",
-                textAlign: 'center'
-            }
+                display: "flex",
+                justifyContent: "",
+            },
         };
     }
-    
+
     handleClick = (buttonClicked) => {
         this.setState({
             style: {
                 width: "80vw",
                 border: "5px solid green",
-                textAlign: buttonClicked
-            }
+                display: "flex",
+                justifyContent: buttonClicked,
+            },
         });
     };
 
-    
     render() {
         return (
             <div
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-            }}
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
             >
                 <h1>Align</h1>
-                <h2
-                    style={this.state.style}
-                    >
-                    Align me like one of your French girls.
-                </h2>
+                <div style={this.state.style}>
+                    <img
+                        src={guyAlign}
+                        style={{ width: "20vw" }}
+                        alt=""
+                    />
+                </div>
+                <h2>Align me like one of your French girls.</h2>
                 <div className="button-container">
-                    <button onClick={() => this.handleClick("left")}>
+                    <button
+                        onClick={() => this.handleClick("flex-start")}
+                    >
                         Left
                     </button>{" "}
-                    <button onClick={() => this.handleClick("center")}>
+                    <button
+                        onClick={() => this.handleClick("center")}
+                    >
                         Center
                     </button>{" "}
-                    <button onClick={() => this.handleClick("right")}>
+                    <button
+                        onClick={() => this.handleClick("flex-end")}
+                    >
                         Right
                     </button>
                 </div>

6a9b3de3154d35e3602733661d2621ae5df2e51a
Brian Kozub <ticet11@gmail.com>
2020-06-01 11:34:18 -0600

    Basic work on toggle.

--- src/pages/Toggle.js
+++ src/pages/Toggle.js
@@ -2,9 +2,8 @@
 
 import React, { Component } from "react";
 
-import gus from '../images/gus.jpg';
-import guy from '../images/guy.jpg';
-
+import gus from "../images/gus.jpg";
+import guy from "../images/guy.jpg";
 
 export default class Toggle extends Component {
     constructor() {
@@ -25,8 +24,21 @@ export default class Toggle extends Component {
         return (
             <div>
                 <h1>Toggle</h1>
-                <button onClick={() => this.handleClick()} >Click to Toggle</button>
-                <div>{this.state.toggleOn ? <img src={guy} alt=""/> : <img src={gus} alt=""/>}</div>
+                <button onClick={() => this.handleClick()}>
+                    Click to Toggle
+                </button>
+                {this.state.toggleOn ? (
+                    <div>
+                        <h2>Guy</h2>
+                        <img src={guy} alt="" />
+                    </div>
+                ) : (
+                    <div>
+                        <h2>Gus</h2>
+                        <img src={gus} alt="" />
+                    </div>
+                )}
+                <a href="https://www.eater.com/2016/8/15/12486666/guy-fieri-without-blonde-hair">Source for Images</a>
             </div>
         );
     }

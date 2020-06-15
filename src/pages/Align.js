457f8f961a078eb8c36e774a8af7286be3498a88
Brian Kozub <ticet11@gmail.com>
2020-06-15 12:06:33 -0600

    Refactored Align component to use hooks.

--- src/pages/Align.js
+++ src/pages/Align.js
@@ -1,70 +1,71 @@
 // change alignment of text
 
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import guyAlign from "../images/guyAlign.jpg";
 
-export default class Align extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            style: {
-                width: "80vw",
-                border: "5px solid green",
+export default function Align() {
+    const [align, setAlign] = useState({
+        width: "80vw",
+        border: "5px solid green",
+        display: "flex",
+        justifyContent: "",
+    });
+    return (
+        <div
+            style={{
                 display: "flex",
-                justifyContent: "",
-            },
-        };
-    }
-
-    handleClick = (buttonClicked) => {
-        this.setState({
-            style: {
-                width: "80vw",
-                border: "5px solid green",
-                display: "flex",
-                justifyContent: buttonClicked,
-            },
-        });
-    };
-
-    render() {
-        return (
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                }}
-            >
-                <h1>Align</h1>
-                <div style={this.state.style}>
-                    <img
-                        src={guyAlign}
-                        style={{ width: "20vw" }}
-                        alt=""
-                    />
-                </div>
-                <h2>Align me like one of your French girls.</h2>
-                <div className="button-container">
-                    <button
-                        onClick={() => this.handleClick("flex-start")}
-                    >
-                        Left
-                    </button>{" "}
-                    <button
-                        onClick={() => this.handleClick("center")}
-                    >
-                        Center
-                    </button>{" "}
-                    <button
-                        onClick={() => this.handleClick("flex-end")}
-                    >
-                        Right
-                    </button>
-                </div>
+                flexDirection: "column",
+                alignItems: "center",
+            }}
+        >
+            <h1>Align</h1>
+            <div style={align}>
+                <img
+                    src={guyAlign}
+                    style={{ width: "20vw" }}
+                    alt=""
+                />
             </div>
-        );
-    }
+            <h2>Align me like one of your French girls.</h2>
+            <div className="button-container">
+                <button
+                    onClick={() =>
+                        setAlign({
+                            width: "80vw",
+                            border: "5px solid green",
+                            display: "flex",
+                            justifyContent: "flex-start",
+                        })
+                    }
+                >
+                    Left
+                </button>{" "}
+                <button
+                    onClick={() =>
+                        setAlign({
+                            width: "80vw",
+                            border: "5px solid green",
+                            display: "flex",
+                            justifyContent: "center",
+                        })
+                    }
+                >
+                    Center
+                </button>{" "}
+                <button
+                    onClick={() =>
+                        setAlign({
+                            width: "80vw",
+                            border: "5px solid green",
+                            display: "flex",
+                            justifyContent: "flex-end",
+                        })
+                    }
+                >
+                    Right
+                </button>
+            </div>
+        </div>
+    );
 }

c704ee20dfcbbe91da22811ee99fc4359ede359d
Brian Kozub <ticet11@gmail.com>
2020-06-15 10:14:31 -0600

    Refactored counter to use hooks.

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -1,51 +1,35 @@
 //TODO: State value -> increase and decrease state value
+// refactored to use hooks
+import React, { useState } from "react";
 
-import React, { Component } from "react";
-
-export default class Counter extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            stateNum: 0,
-        };
-    }
-
-    handleButtonClick = (number) => {
-        this.setState((prevState) => ({
-            stateNum: prevState.stateNum + number,
-        }));
-    };
-
-    render() {
-        return (
-            <div style={{ padding: "10px" }}>
-                <h1>Counter</h1>
-                <div
-                    className="counter-container"
-                    style={{
-                        width: "100px",
-                        padding: "5px",
-                        display: "grid",
-                        gridTemplateColumns: "1fr 1fr",
-                        alignItems: "center",
-                    }}
-                >
-                    <h2>{this.state.stateNum}</h2>
-                    <div className="button-container">
-                        <button
-                            onClick={() => this.handleButtonClick(1)}
-                        >
-                            Up
-                        </button>
-                        <button
-                            onClick={() => this.handleButtonClick(-1)}
-                        >
-                            Down
-                        </button>
-                    </div>
+function Counter() {
+    const [count, setCount] = useState(0);
+    return (
+        <div style={{ padding: "10px" }}>
+            <h1>Counter</h1>
+            <div
+                className="counter-container"
+                style={{
+                    width: "100px",
+                    padding: "5px",
+                    display: "grid",
+                    gridTemplateColumns: "1fr 1fr",
+                    alignItems: "center",
+                }}
+            >
+                <p>{count}</p>
+                <div className="button-container">
+                    <button onClick={() => setCount(count + 1)}>
+                        Up
+                    </button>
+                    <button onClick={() => setCount(count - 1)}>
+                        Down
+                    </button>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
+export default Counter;
+
